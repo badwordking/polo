ceeb674278438f2ab52516ce521a7c65e930975f
badwordking <51959848+badwordking@users.noreply.github.com>
2019-11-16 23:30:36 +0700

    Add files via upload

---
+++ icc.php
@@ -0,0 +1,8 @@
+<?php
+
+//Refresh Toket
+$autocred = "AEu4IL2W4trLYFC6Tdn0I1IYd0nGxzavfUfHdo07dSDOecDKnwnWLi-xuPxkI88U4zhCad37ce1GUij34Vtiy3eYYYvsAsQs9XuFRS0iHaOCj39akPA29BMiFY2f3qMijEiftjf_f4yFMtDwIDl5ZSlOGX3wzS6zfmFplL61oF4j8BbyrDmb1MdDBEfSw_c8dOMySY0YN6tzyUhWJxD3LY_Cw_Zh5neqzKG5Vu1wxm2uzJtoUjvpBcYZe3pYScH4IMn8soTFSAY4anTBjPT3rBNudNcRT7J9kJ6t_Ko2khQTzrxj53qnBRI8QcRKgthcYdYKFoJiYg6Lvlu6X3-zpYUd9f_ZUmR7mGlHvqt3Vx8lWgEBFVB87--Gx3mO_NReEPKqxzxu0Sb2dnd_oCRkfOFGzZbCfu_MUu7ic4BJo0a9EnSMRHuMhEQ";
+
+//Device Id kalau bingung isi acak saja 
+$deviceid = "3b125d54445c354d";
+
